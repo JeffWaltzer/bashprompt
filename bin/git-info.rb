8e0a91d33135992517f2d21eed83f03a2c73d126
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-12-30 15:08:36 -0500

    refactorings, cleaned up added spaces

--- bin/git-info.rb
+++ bin/git-info.rb
@@ -1,33 +1,33 @@
 #!/usr/bin/ruby
 
 COLORS = {
-    black: 0,
-    red: 1,
-    green: 2,
-    yellow: 3,
-    blue: 4,
-    magenta: 5,
-    cyan: 6,
-    white: 7,
-    default: 9,
+  black: 0,
+  red: 1,
+  green: 2,
+  yellow: 3,
+  blue: 4,
+  magenta: 5,
+  cyan: 6,
+  white: 7,
+  default: 9,
 
-    light_black: 10,
-    light_red: 11,
-    light_green: 12,
-    light_yellow: 13,
-    light_blue: 14,
-    light_magenta: 15,
-    light_cyan: 16,
-    light_white: 17
+  light_black: 10,
+  light_red: 11,
+  light_green: 12,
+  light_yellow: 13,
+  light_blue: 14,
+  light_magenta: 15,
+  light_cyan: 16,
+  light_white: 17
 }
 
 MODES = {
-    default: 0, # Turn off all attributes
-    bold: 1, # Set bold mode
-    underline: 4, # Set underline mode
-    blink: 5, # Set blink mode
-    swap: 7, # Exchange foreground and background colors
-    hide: 8 # Hide text (foreground color would be the same as background)
+  default: 0, # Turn off all attributes
+  bold: 1, # Set bold mode
+  underline: 4, # Set underline mode
+  blink: 5, # Set blink mode
+  swap: 7, # Exchange foreground and background colors
+  hide: 8 # Hide text (foreground color would be the same as background)
 }
 
 
@@ -50,7 +50,7 @@ end
 
 def show_status
   s=`git status -s | cut -c-2 | sort | uniq -c | tr "\\n" ":" | tr -s " "`
-  s.size==0 ? '' : "#{color :blue}#{s.strip}#{color :green}"
+  s.size==0 ? '' : "(#{color :blue}#{s.strip}#{color :green})"
 end
 
 def show_files
@@ -67,7 +67,15 @@ def show_branch
 end
 
 if Dir.exists?('.git')
-  puts "#{color :green}#{show_rvm} (#{show_status}) #{show_files} #{show_stash} #{show_branch}#{color_reset}".squeeze
+  puts color(:green) +
+         [
+           show_rvm,
+           show_status,
+           show_files,
+           show_stash,
+           show_branch,
+         ].join(' ').squeeze +
+         color_reset
 else
   puts show_rvm.chomp
 end
